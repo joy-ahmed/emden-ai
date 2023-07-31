862f669d8da9069a970c89a8a2f19f2c069b647c
Joy Ahmed <dev.joyahmed@gmail.com>
2023-07-31 17:24:39 +0600

    styled up login/signup page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen flex items-center justify-center">{children}</div>
