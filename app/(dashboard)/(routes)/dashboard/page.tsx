6facfba78b8738148b4558a6e9c7f4cf22aa7521
Joy Ahmed <dev.joyahmed@gmail.com>
2023-07-31 15:15:15 +0600

    added clerk auth pkg

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-indigo-400">Dashboard</h1>
+      <Button>Click me </Button>
+    </div>
+  );
+}
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl">Hi Mom!</h1>
+      <h1 className="text-2xl text-teal-400">Hi Mom!</h1>
       <Button>Click me </Button>
     </div>
   );
