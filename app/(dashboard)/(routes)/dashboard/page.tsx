a90be4a594b05780b704d0fbc1e249a9aef3daba
Joy Ahmed <dev.joyahmed@gmail.com>
2023-08-01 00:48:46 +0600

    dashboard page covered

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,87 @@
-export default function Home() {
+"use client";
+
+import {
+  ArrowRight,
+  FileCode2,
+  Image,
+  MessageSquare,
+  Music,
+  Video,
+} from "lucide-react";
+import React from "react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Image Generator",
+    icon: Image,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generator",
+    icon: Video,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/video",
+  },
+  {
+    label: "Music Generator",
+    icon: Music,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/music",
+  },
+  {
+    label: "Code Generator",
+    icon: FileCode2,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/code",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+];
+
+const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <h1 className="text-2xl text-indigo-400">Dashboard</h1>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          A Generative AI tools for the next.
+        </h2>
+        <p className="text-center text-muted-foreground font-light text-sm md:text-lg">
+          Interect with the clever AI and get job done!
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default DashboardPage;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,6 +14,7 @@ import { Audiowide } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const audiowide = Audiowide({
   weight: "400",
@@ -65,6 +66,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className=" space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -79,7 +81,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-sm transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-sm transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
