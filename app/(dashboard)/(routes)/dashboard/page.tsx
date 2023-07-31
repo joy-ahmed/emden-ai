0385c8b00f4fcf1feee3e63e5a1e9ace0aaf537f
Joy Ahmed <dev.joyahmed@gmail.com>
2023-07-31 18:05:54 +0600

    created navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl text-indigo-400">Dashboard</h1>
-      <Button>Click me </Button>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
